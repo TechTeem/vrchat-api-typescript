b7e140de42939d2fc9ad8eb7e401636fa31b4a61
Ferdinand Lim <limhjf@gmail.com>
2026-03-13 00:06:09 +0800

    Release 0.1.2

--- src/spec-meta.ts
+++ src/spec-meta.ts
@@ -1,3 +1,3 @@
 export const VRCHAT_SPEC_URL = "https://github.com/vrchatapi/specification/releases/latest/download/openapi.json";
 export const VRCHAT_SPEC_VERSION = "1.20.7";
-export const VRCHAT_SPEC_GENERATED_AT = "2026-03-12T15:58:04.991Z";
+export const VRCHAT_SPEC_GENERATED_AT = "2026-03-12T16:05:14.786Z";
